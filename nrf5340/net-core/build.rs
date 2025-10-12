d7e14243c77751f631277a9a5c6349ed9570119a
Matthew Tran <0e4ef622@gmail.com>
2025-10-11 23:56:09 -0500

    Use memory-spec in nrf5340

--- nrf5340/net-core/build.rs
+++ nrf5340/net-core/build.rs
@@ -12,25 +12,49 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
+use std::str::FromStr;
+
+use memory_spec::MemorySpec;
+
+fn memory_x() -> String {
+    let content = std::fs::read_to_string("../memory.kdl").unwrap();
+    let memoryspec = MemorySpec::from_str(&content).unwrap();
+
+    let flash = &memoryspec.regions()["netcore_flash"];
+    let flash_origin = flash.origin();
+    let flash_length = flash.length();
+    let ram = &memoryspec.regions()["netcore_ram"];
+    let ram_origin = ram.origin();
+    let ram_length = ram.length();
+    let symbols = memoryspec.render_symbols();
+    format!(
+        "\
+MEMORY
+{{
+  FLASH : ORIGIN = 0x{flash_origin:08x}, LENGTH = 0x{flash_length:04x}
+  RAM : ORIGIN = 0x{ram_origin:08x}, LENGTH = 0x{ram_length:04x}
+}}
+{symbols}"
+    )
+}
 
 fn main() {
-    let memory_x_bytes = include_bytes!("memory.x");
+    let memory_x_content = memory_x();
 
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out.join("memory.x"))
         .unwrap()
-        .write_all(memory_x_bytes)
+        .write_all(memory_x_content.as_bytes())
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 
     // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
+    // any file in the project changes. By specifying `memory.kdl`
     // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory-app.x");
-    println!("cargo:rerun-if-changed=memory-net.x");
+    // `memory.kdl` is changed.
+    println!("cargo:rerun-if-changed=../memory.kdl");
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
